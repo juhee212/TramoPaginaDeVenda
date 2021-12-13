fb8f580550053ad7eecd5c4817a5c46e9d616c52
Julia Santana <juliahelenalimadesantana@gmail.com>
2021-12-13 19:13:04 -0300

    Primeiro comentário

---
+++ Js/main.js
@@ -0,0 +1,22 @@
+const storeProducts = document.querySelectorAll('.store-product');
+
+// FILTRO DE PROCURA
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".product-details h4", ".product-details h5");
+
+// A BETTER WAY TO FILTER THROUGH THE PRODUCTS
+search.addEventListener("keyup", filterProducts);
+
+
+function filterProducts(e) {
+    const text = e.target.value.toLowerCase();
+    // console.log(productName[0]);
+    productName.forEach(function(product) {
+        const item = product.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            product.parentElement.parentElement.style.display = "block"
+        } else {
+            product.parentElement.parentElement.style.display = "none"
+        }
+    })
+}
---
+++ Js/script.js
@@ -0,0 +1,11 @@
+
+//zoom
+$(function () {
+    $(".xzoom, .xzoom-gallery").xzoom({
+        zoomWidth: 200,
+        zoomHeight: 200,
+        tint: "#333",
+        Xoffset: 15,
+        lens: true,
+    })
+});
